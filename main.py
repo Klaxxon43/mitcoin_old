60ea26aaa05d30b9ca681819101b2dee7870082a
1xxwee <155847901+1xxwee@users.noreply.github.com>
2025-02-05 18:17:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,45 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums.parse_mode import ParseMode
-from config import API_TOKEN
+from config import *
 from client import client, start_background_tasks
 from admin import admin
 from db import DB
 from aiogram.client.default import DefaultBotProperties
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 logging.basicConfig(level=logging.INFO)
 
-async def main():
+
+# Функция, которая будет вызвана при запуске бота
+async def on_startup(bot: Bot) -> None:
+    await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
+    await DB.create()
+    await start_background_tasks(bot, DB)
+
+# Функция, которая будет вызвана при остановке бота
+async def on_shutdown(bot: Bot) -> None:
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.session.close()
+
+
+def main():
     bot = Bot(API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
     dp.include_routers(client, admin)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await DB.create()
-    await start_background_tasks(bot, DB)
-    await dp.start_polling(bot)
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,  
+        bot=bot  
+    )
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host=HOST, port=PORT)
 
 
-asyncio.run(main())
+main()
+
