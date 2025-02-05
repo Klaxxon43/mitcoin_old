57762a1d76b7a6038c1f5ba1096f55cc3734f7b5
1xxwee <155847901+1xxwee@users.noreply.github.com>
2025-02-05 18:15:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 # Задаем переменные окружения
 API_TOKEN = os.environ["API_TOKEN"]
 CRYPTOBOT_TOKEN = os.environ["CRYPTOBOT_TOKEN"]
-
+HOST='0.0.0.0'
+PORT=8000
+BASE_URL='' #можно через ngrok
+WEBHOOK_PATH = f'/{API_TOKEN}'
 
 ADMINS_ID = [996459546, 2118524898, 6806012624]
